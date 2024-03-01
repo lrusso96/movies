3f7f3bf1df49f5e4a37b88800c9184ae40d9bbd8
lrusso96 <russol@eurecom.fr>
2024-03-01 19:31:33 +0100

    Drop Jquery

--- assets/js/infinite-jekyll.js
+++ assets/js/infinite-jekyll.js
@@ -1,91 +1,81 @@
-$(function() {
-  
-    var posts,
-        isFetchingPosts = false,
-        shouldFetchPosts = true;
-  
-    var postsToLoad = $(".tag-master:not(.hidden) .post-list").children().length, loadNewPostsThreshold = 200;
+document.addEventListener("DOMContentLoaded", async () => {
+  let posts;
+  let isFetchingPosts = false;
+  let shouldFetchPosts = true;
+  let spinner = document.getElementById("infinite-spinner");
 
-    $.getJSON('/movies/all-posts.json', function(data) {
-      posts = data["posts"];
-      // If there aren't any more posts available to load than already visible, disable fetching
-      if (posts.length <= postsToLoad)
-        disableFetching();
-    });
-  
-  
-    // If there's no spinner, it's not a page where posts should be fetched    
-    if ($(".infinite-spinner").length < 1)
-      shouldFetchPosts = false;
-  
-    // Are we close to the end of the page? If we are, load more posts
-    $(window).scroll(function(e){
-      if (!shouldFetchPosts || isFetchingPosts) return;
-  
-      var windowHeight = $(window).height(),
-          windowScrollPosition = $(window).scrollTop(),
-          bottomScrollPosition = windowHeight + windowScrollPosition,
-          documentHeight = $(document).height();
-  
-      // If we've scrolled past the loadNewPostsThreshold, fetch posts
-      if ((documentHeight - loadNewPostsThreshold) < bottomScrollPosition) {
-        fetchPosts();
-      }
-    });
-  
-    // Fetch a chunk of posts
-    function fetchPosts() {
-      // Exit if posts haven't been loaded
-      if (!posts) return;
-  
-      isFetchingPosts = true;
-  
-      // Load as many posts as there were present on the page when it loaded
-      // After successfully loading a post, load the next one
-      var loadedPosts = 0,
-          postCount = $(".tag-master:not(.hidden) .post-list").children().length,
-          callback = function() {
-            loadedPosts++;
-            var postIndex = postCount + loadedPosts;
-  
-            if (postIndex > posts.length-1) {
-              disableFetching();
-              $(".infinite-spinner").hide()
-              return;
-            }
-  
-            if (loadedPosts < postsToLoad) {
-              fetchPostWithIndex(postIndex, callback);
-            } else {
-              isFetchingPosts = false;
-            }
-          };
-  
-      fetchPostWithIndex(postCount + loadedPosts, callback);
-    }
-  
-    function fetchPostWithIndex(index, callback) {
-      let post = posts[index];
+  // If there's no spinner, it's not a page where posts should be fetched   
+  if (!spinner) shouldFetchPosts = false;
 
-      post = $.parseHTML(`<div class="group relative flex flex-row h-auto min-w-full rounded-lg shadow post">
-      <a href="${post.url}" class="overflow-hidden grow">
-        <img loading="lazy" class="min-w-full transition duration-300 ease-in-out group-hover:scale-110 brightness-[0.7] group-hover:brightness-50" src="${post.cover}" alt="" />
-        <div class="absolute bottom-0 left-0 p-6 xl:p-8 2xl:p-12 ">
-          <h1 class="text-white group-hover:text-amber-300 text-xl md:text-2xl xl:text-3xl 3xl:text-4xl font-extrabold uppercase ">${post.title}</h1>
-          <p class="text-white group-hover:text-amber-300 md:mt-2 text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl font-bold">
-            ${post.director} · ${post.year}
-          </p>
-        </div>
-        </a>
-  </div>`)
-      $(".post-list").append(post)
-      callback()
+  let postList = document.getElementById("post-list");
+  let postsToLoad = postList.children.length;
+  let loadNewPostsThreshold = 200;
+
+  let data = await fetch("/movies/all-posts.json");
+  data = await data.json();
+  posts = data["posts"].slice(0, 50);
+
+  // If there aren't any more posts available to load than already visible, disable fetching
+  if (posts.length <= postsToLoad) disableFetching();
+
+  // Load more posts if close to the end of the page
+  window.addEventListener("scroll", function () {
+    if (!shouldFetchPosts || isFetchingPosts) return;
+
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - loadNewPostsThreshold) {
+      fetchPosts();
     }
-    
-    function disableFetching() {
-      shouldFetchPosts = false;
-      isFetchingPosts = false;
-      $(".infinite-spinner").fadeOut();
-    }
-      
   });
+
+  function fetchPosts() {
+    if (!posts) return;
+
+    isFetchingPosts = true;
+
+    let loadedPosts = 0;
+    let postCount = postList.children.length;
+    let callback = function () {
+      loadedPosts++;
+      let postIndex = postCount + loadedPosts;
+
+      if (postIndex > posts.length - 1) {
+        disableFetching();
+        spinner.style.display = "none";
+        return;
+      }
+
+      if (loadedPosts < postsToLoad) {
+        fetchPostWithIndex(postIndex, callback);
+      } else {
+        isFetchingPosts = false;
+      }
+    };
+
+    fetchPostWithIndex(postCount + loadedPosts, callback);
+  }
+
+  function fetchPostWithIndex(index, callback) {
+    let post = posts[index];
+    post = `<div class="group relative flex flex-row h-auto min-w-full rounded-lg shadow post">
+    <a href="${post.url}" class="overflow-hidden grow">
+      <img loading="lazy" class="min-w-full transition duration-300 ease-in-out group-hover:scale-110 brightness-[0.7] group-hover:brightness-50" src="${post.cover}" alt="" />
+      <div class="absolute bottom-0 left-0 p-6 xl:p-8 2xl:p-12 ">
+        <h1 class="text-white group-hover:text-amber-300 text-xl md:text-2xl xl:text-3xl 3xl:text-4xl font-extrabold uppercase ">${post.title}</h1>
+        <p class="text-white group-hover:text-amber-300 md:mt-2 text-lg md:text-lg lg:text-lg xl:text-xl 2xl:text-2xl font-bold">
+          ${post.director} · ${post.year}
+        </p>
+      </div>
+      </a>
+      </div>`;
+    let template = document.createElement("template");
+    template.innerHTML = post;
+    postList.appendChild(template.content.children[0]);
+    callback();
+  }
+
+  function disableFetching() {
+    shouldFetchPosts = false;
+    isFetchingPosts = false;
+    spinner.remove();
+  }
+});
